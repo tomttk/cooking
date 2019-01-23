1b63f3d723d3d15527bb8b0f97ac7ccce5c1daaf
Tom Tual-Krebs <tualkrebstom@gmail.com>
2019-01-23 14:31:28 +0100

    Add files via upload

--- auth.php
+++ auth.php
@@ -1,2 +1,6 @@
 ﻿<?php
 $pdo = new PDO('mysql:host=localhost;dbname=site_cooking', 'root', '', array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+
+// change encodage
+$mysqli = new mysqli("localhost", "root", "", "site_cooking");
+$mysqli->set_charset("utf8");
